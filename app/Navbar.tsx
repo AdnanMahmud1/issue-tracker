0555cfcedf1c3c6fe02b225102753ed70b4cc212
AdnanMahmud1 <11adnan@live.com>
2024-04-03 01:29:30 +0600

    first commit

---
+++ app/Navbar.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import React from "react";
+import { AiFillBug } from "react-icons/ai";
+
+const NavBar = () => {
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-6">
+        <li>
+          <Link href="/">Dashboard</Link>
+        </li>
+        <li>
+          <Link href="/issues">Issues</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
