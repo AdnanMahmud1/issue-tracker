c9835bcf743b6c9cd0190caddac2cda1160817a0
Adnan <Adnan@DESKTOP-UGPNVH0>
2024-04-08 12:19:50 +0600

    Build the new issue page

---
+++ app/issues/new/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { TextArea, TextField, Button } from "@radix-ui/themes";
+import React from "react";
+
+const NewIssuePage = () => {
+  return (
+    <div className="max-w-xl space-y-3">
+      <TextField.Root placeholder="Title"></TextField.Root>
+      <TextArea placeholder="Description" />
+      <Button>Submit</Button>
+
+    </div>
+  );
+};
+
+export default NewIssuePage;
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,12 @@
 import { Button } from "@radix-ui/themes";
+import Link from "next/link";
 
 const IssuesPage = () => {
-  return <Button>New Issue</Button>;
+  return (
+    <Button>
+      <Link href="/issues/new">New Issue</Link>
+    </Button>
+  );
 };
 
 export default IssuesPage;
